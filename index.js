77efbe373683ef3af37aa12a0c8045db975454b7
Mahfuz <you@example.com>
2021-01-30 03:13:47 +0600

    code up

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function getFirstClassTicket(id,isIncrease) {
     newTicketCount = ticketCount - 1;
   }
   ticketInput.value = newTicketCount;
+  totalTicketCost();
   
 }
 
@@ -31,7 +32,28 @@ document.getElementById( "firstClass-minus").addEventListener("click", function 
     getFirstClassTicket("economy-count",false);
 })
 
-//
+//total ticket cost calculate
+function totalTicketCost(){
+  const firstClassCount=getInputValue("firstClass-count");
+  const economyClassCount=getInputValue("economy-count");
+  const subTotalCount=firstClassCount*150+economyClassCount*100;
+  document.getElementById("subtotal-amount").innerText=subTotalCount;
+  const taxAmount=Math.round(subTotalCount/100*10);
+  document.getElementById("tax-amount").innerText=taxAmount;
+  const totalAmount=subTotalCount+taxAmount;
+  document.getElementById("total-amount").innerText=totalAmount;
+
+
+}
+
+function getInputValue(id){
+  const ticketInput=document.getElementById(id);
+  const ticketCount=parseInt(ticketInput.value);
+  return ticketCount;
+ 
+
+}
+
 
 
  
